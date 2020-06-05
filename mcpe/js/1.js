b0f5dd9b1eabcd843d0c3d969c77c915e53e3fa6
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-06-05 12:38:27 -0300

    a/*

---
+++ mcpe/js/1.js
@@ -0,0 +1,18 @@
+/* =======================================
+=           typing animation             =
+=========================================
+
+function Typer(e) {
+    var listaText = e.innerHTML.split("");
+    e.innerHTML = "";
+    listaText.forEach(function (letra, i) {
+        setTimeout(function () {
+            e.innerHTML += letra;
+        }, 35 * i)
+    })
+}
+
+var subtitulo = document.querySelector("h3");
+Typer(subtitulo);
+
+*/
---
+++ old/beta/cru/index2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<title>Matheus 23 blog</title>
+<body>
+ <h2> Ola </h2>
+
+
+</body>
+</html>
+
